692bbaa5f6c0a12a00457bb7c017de2d29f70b83
Jose F <josevictorjop@gmail.com>
2024-11-13 20:36:22 -0300

    Environment Variable hell 2

--- replace-env-vars.js
+++ replace-env-vars.js
@@ -1,20 +1,30 @@
 const fs = require('fs');
-const path = './dist/scumbags/assets/environment.js';
+const path = './dist/your-project-name/assets/environment.vercel.js';
 
-console.log("Injecting environment variables...");
+console.log("Injecting Vercel environment variables...");
 
 fs.readFile(path, 'utf8', function (err, data) {
-  if (err) return console.error("Error reading file:", err);
+  if (err) return console.error("Error reading environment file:", err);
 
-  // Replace placeholders with Vercel environment variables
   let result = data
-    .replace(/VITE_EMAILJS_USER_ID/g, process.env.VITE_EMAILJS_USER_ID)
-    .replace(/VITE_EMAILJS_SERVICE_ID/g, process.env.VITE_EMAILJS_SERVICE_ID)
-    .replace(/VITE_EMAILJS_TEMPLATE_ID/g, process.env.VITE_EMAILJS_TEMPLATE_ID)
-    .replace(/VITE_EMAILJS_TEMPLATE_2_ID/g, process.env.VITE_EMAILJS_TEMPLATE_2_ID);
+    .replace(/''/g, (match, offset) => {
+      // Inject Vercel variables
+      switch (offset) {
+        case data.indexOf("emailServiceApiKey: ''"):
+          return `'${process.env.VITE_EMAILJS_USER_ID}'`;
+        case data.indexOf("serviceId: ''"):
+          return `'${process.env.VITE_EMAILJS_SERVICE_ID}'`;
+        case data.indexOf("templateId: ''"):
+          return `'${process.env.VITE_EMAILJS_TEMPLATE_ID}'`;
+        case data.indexOf("template2Id: ''"):
+          return `'${process.env.VITE_EMAILJS_TEMPLATE_2_ID}'`;
+        default:
+          return match;
+      }
+    });
 
   fs.writeFile(path, result, 'utf8', function (err) {
-    if (err) return console.error("Error writing file:", err);
+    if (err) return console.error("Error writing environment file:", err);
     console.log("Environment variables injected successfully.");
   });
 });
---
+++ src/environments/environment.vercel.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    emailServiceApiKey: '',
+    serviceId: '',
+    templateId: '',
+    template2Id: ''
+  };
+  
