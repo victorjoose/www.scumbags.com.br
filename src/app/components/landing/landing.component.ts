1337cff78958419a18c3717380bb019d65704ffa
Jose F <josevictorjop@gmail.com>
2024-11-13 18:28:24 -0300

    Configuração vídeo, env placeholder

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
+  @ViewChild('backgroundVideo') backgroundVideo!: ElementRef;
+  videoStarted = false;
+
+  ngAfterViewInit() {
+    this.loadAndPlayVideo();
+  }
+
+  private loadAndPlayVideo() {
+    const video: HTMLVideoElement = this.backgroundVideo.nativeElement;
+
+    video.muted = true;
+    video.loop = true;
+
+    video.play().then(() => {
+      this.videoStarted = true;
+    }).catch();
+  }
 
 }
