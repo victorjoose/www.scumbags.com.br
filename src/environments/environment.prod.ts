c9481fb37eb38e3be8aad6a25a4aa9a44b261b23
Jose F <josevictorjop@gmail.com>
2024-11-13 21:39:26 -0300

    Environment Variable hell 5

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  emailServiceApiKey: 'VITE_EMAILJS_USER_ID',
-  serviceId: 'VITE_EMAILJS_SERVICE_ID',
-  templateId: 'VITE_EMAILJS_TEMPLATE_ID',
-  template2Id: 'VITE_EMAILJS_TEMPLATE_2_ID'
-};
