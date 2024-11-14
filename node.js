b31761a4082fbc051fa0bc0d67060da2b622dad6
Jose F <josevictorjop@gmail.com>
2024-11-13 21:51:54 -0300

    Environment Variable hell 6

--- node.js
+++ node.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
-const dotenv = require('dotenv').config({path: 'src/.env'}); ;
+require('dotenv').config({ path: 'src/.env' });
 
 const envFile = `export const environment = {
-    emailServiceApiKey: '${process.env.VITE_EMAILJS_USER_ID}',
-    serviceId: '${process.env.VITE_EMAILJS_SERVICE_ID}',
-    templateId: '${process.env.VITE_EMAILJS_TEMPLATE_ID}',
-    template2Id: '${process.env.VITE_EMAILJS_TEMPLATE_2_ID}',
+    VITE_EMAILJS_USER_ID: '${process.env.VITE_EMAILJS_USER_ID}',
+    VITE_EMAILJS_SERVICE_ID: '${process.env.VITE_EMAILJS_SERVICE_ID}',
+    VITE_EMAILJS_TEMPLATE_ID: '${process.env.VITE_EMAILJS_TEMPLATE_ID}',
+    VITE_EMAILJS_TEMPLATE_2_ID: '${process.env.VITE_EMAILJS_TEMPLATE_2_ID}',
 };`;
 
 const targetPath = path.join(__dirname, './src/environments/environment.development.ts');
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    VITE_EMAILJS_USER_ID: '',
+    VITE_EMAILJS_SERVICE_ID: '',
+    VITE_EMAILJS_TEMPLATE_ID: '',
+    VITE_EMAILJS_TEMPLATE_2_ID: ''
+  };
