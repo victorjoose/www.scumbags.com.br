93f5a2c611954473121d2f9767e9c2387db2ce74
Jose F <josevictorjop@gmail.com>
2024-12-12 16:16:21 -0300

    correções visuais, adicionado botão de tradução para mobile

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    VITE_EMAILJS_USER_ID: '',
-    VITE_EMAILJS_SERVICE_ID: '',
-    VITE_EMAILJS_TEMPLATE_ID: '',
-    VITE_EMAILJS_TEMPLATE_2_ID: '',
+    VITE_EMAILJS_USER_ID: 'undefined',
+    VITE_EMAILJS_SERVICE_ID: 'undefined',
+    VITE_EMAILJS_TEMPLATE_ID: 'undefined',
+    VITE_EMAILJS_TEMPLATE_2_ID: 'undefined',
 };
