5328b1f773d6bdee544b3bd913d340043af0ef4f
Jose F <josevictorjop@gmail.com>
2025-05-26 03:29:03 -0300

    Add cart, full checkout form with validations, and responsive layout improvements

--- src/app/components/product-modal/product-modal.component.ts
+++ src/app/components/product-modal/product-modal.component.ts
@@ -1,20 +1,43 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, ViewChild } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { TranslateService } from '@ngx-translate/core';
+import { ToastComponent } from '../toast/toast.component';
 
 @Component({
   selector: 'app-product-modal',
   templateUrl: './product-modal.component.html',
   styleUrls: ['./product-modal.component.css']
 })
-export class ProductModalComponent {
+export class ProductModalComponent implements OnInit {
   @Input() product: any;
   @Output() close = new EventEmitter<void>();
   selectedSize: string = '';
   currentImage: string = '';
+  currencyCode: string = 'BRL';
+  @ViewChild('toast') toast!: ToastComponent;
+
+  constructor(
+    private cartService: CartService,
+    private translate: TranslateService
+  ) {}
 
   ngOnInit() {
     if (this.product) {
       this.currentImage = this.product.imageUrl;
     }
+
+    this.setCurrencyCode(this.translate.currentLang);
+    this.translate.onLangChange.subscribe(lang => {
+      this.setCurrencyCode(lang.lang);
+    });
+  }
+
+  setCurrencyCode(lang: string) {
+    this.currencyCode = lang === 'pt' ? 'BRL' : 'USD';
+  }
+
+  getProductPrice(): number {
+    return this.currencyCode === 'BRL' ? this.product.priceBrl : this.product.priceUsd;
   }
 
   closeModal() {
@@ -22,10 +45,15 @@ export class ProductModalComponent {
   }
 
   toggleImage() {
-    if (this.currentImage === this.product.imageUrl) {
-      this.currentImage = this.product.imageUrl2;
-    } else {
-      this.currentImage = this.product.imageUrl;
+    this.currentImage =
+      this.currentImage === this.product.imageUrl ? this.product.imageUrl2 : this.product.imageUrl;
+  }
+
+  addToCart() {
+    if (this.selectedSize) {
+      this.cartService.addToCart(this.product, this.selectedSize);
+      this.toast?.show('Item adicionado ao carrinho!');
+      this.closeModal();
     }
   }
 }
