2d1e1e18eaaab1888cf7e23bcd9a366d58bcf279
Jose F <josevictorjop@gmail.com>
2025-05-26 21:03:26 -0300

    Fix: Unit tests

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,8 +10,17 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [HomeComponent],
+      providers: [{
+        provide: ActivatedRoute,
+        useValue: {
+          params: of({}),
+          queryParams: of({ product: 1 })
+        }}
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     });
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
