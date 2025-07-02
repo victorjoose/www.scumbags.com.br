a0db5e973c7967cdff952d9c48fee866e1814fea
Jose F <josevictorjop@gmail.com>
2025-07-02 02:36:35 -0300

    environment fix

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  MOCK_EMAIL: false,
+  VITE_EMAILJS_USER_ID: '',
+  VITE_EMAILJS_SERVICE_ID: '',
+  VITE_EMAILJS_TEMPLATE_ID: '',
+  VITE_EMAILJS_TEMPLATE_2_ID: '',
+  SUPABASE_URL: '',
+  SUPABASE_ANON_KEY: '',
+};
